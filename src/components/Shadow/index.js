55da65047a4eabed2a42baed24cc3154e1ecaffd
leafartech <119262053+leafartech@users.noreply.github.com>
2023-05-25 17:16:49 -0300

    Delete src directory

--- src/components/Shadow/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Shadow.module.css'
-
-export default function Shadows() {
-    return (
-        <div className={ styles.shadows }>
-            {/* CONFIGURAR "SOMBRAS" para aparecerem enquanto a página carrega*/}
-        </div>
-    )
-}
