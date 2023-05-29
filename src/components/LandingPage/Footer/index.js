aa1d70ff02c326cdf91d905e86cf4bd93778183a
leafartech <119262053+leafartech@users.noreply.github.com>
2023-05-29 08:22:26 -0300

    Delete src directory

--- src/components/LandingPage/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css'
-
-export default function Footer({}) {
-    return (
-        <footer className={ styles.rodape }>
-            <p>Desenvolvido por Rafael Borges Bezerra</p>
-        </footer>
-    )
-}
--- src/components/LandingPage/Section/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Section.module.css'
-
-export default function Section({ children }) {
-    return (
-        <section className={ styles.section }>
-            {children}
-        </section>
-    )
-}
